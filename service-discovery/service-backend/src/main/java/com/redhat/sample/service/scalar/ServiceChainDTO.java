2646c0a90c7d4a2221acf9f26dbebf399b5d6b92
Tham Hon Hoe <honhoe.tham@sc.com>
2017-09-01 21:32:05 +0800

    test

---
+++ service-discovery/service-backend/src/main/java/com/redhat/sample/service/scalar/ServiceChainDTO.java
@@ -0,0 +1,17 @@
+package com.redhat.sample.service.scalar;
+
+public class ServiceChainDTO {
+	
+	private String greeting;
+	public String getGreeting() {return greeting;}
+	public void setGreeting(String greeting) {this.greeting = greeting;}
+	
+	private long time;
+	public long getTime() {return time;}
+	public void setTime(long time) {this.time = time;}
+	
+	private String ip;
+	public String getIp() {return ip;}
+	public void setIp(String ip) {this.ip = ip;}
+	
+}
--- service-discovery/service-hello/src/main/java/com/redhat/sample/service/GreetRestService.java
+++ service-discovery/service-hello/src/main/java/com/redhat/sample/service/GreetRestService.java
@@ -1,18 +1,35 @@
 package com.redhat.sample.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.redhat.sample.service.scalar.ServiceChainDTO;
 
 @RestController
 @RequestMapping("/api/greet")
 public class GreetRestService {
+	
+	private final Logger log = LoggerFactory.getLogger(GreetRestService.class);
+	
+	@Autowired
+	private Environment env;
+	private RestTemplate restfulTemplate = new RestTemplate();
 
+	
 	
 	@RequestMapping(value = "/kube-dns/plain", method = RequestMethod.GET, produces = "text/plain")
 	public String greeting(){
 		
-		return "hello world";
+		log.info("Stepping into Gretting");
+		log.info("calling backend : " + this.env.getProperty("backend.chaincall.plain"));
+		ServiceChainDTO response = restfulTemplate.getForObject(this.env.getProperty("backend.chaincall.plain"), ServiceChainDTO.class);
+		return "hello world " +  response.getGreeting();
 	}
 	
 }
